4e9cf7653b508e130e8391fceef33692422fd70d
root <root@DESKTOP-0LKRV38.fios-router.home>
2017-02-01 16:45:04 -0500

    init commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Title:		Promotional Website
+ * Author:		Steven Roomberg
+ * Description:	This is a promo website I've developed for my CS1520 course at the Univ of Pittsburgh.
+ * */
+
+include 'header.php';
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+	  <title>Steven Roomberg</title>
+	  <link rel="stylesheet" type="text/css" href="style/style.css">
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	</head>
+	<body>
+		<!--Beginning of stuff-->
+	</body>
+	<script type="text/javascript" src="script/script.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</html>
