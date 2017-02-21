6ce0f4ec67f12a63e3bc8e970a4e36c1eee12f21
Steven R <stevenroomberg@gmail.com>
2017-02-21 11:08:05 -0500

    fixed navigation anchors, added some styles, fixed layout, added portfolio section, ...

--- src/index.php
+++ src/index.php
@@ -18,44 +18,73 @@
 			<div class="row">
 				<!--Navigation section | Pinned to left side of page as user scrolls-->
 				<div class="col-md-4 navigation">
-					<ul>
+					<ul class="center-block text-center">
 						<li><a href="#top">Home</a></li>
 						<li><a href="#about">About</a></li>
+						<li><a href="#education">Education</a></li>
 						<li><a href="#resume">Resume</a></li>
+						<li><a href="#portfolio">Portfolio</a></li>
 						<li><a href="#contact">Contact</a></li>
 					</ul>
+					<div class="social-links">
+						<!--Social Links-->
+						<ul></ul>
+					</div>
 				</div>
 				<!--Information section | Will be a full-page scroll-->
 				<div class="col-md-8 info">
+					<a name="top"></a>
 					<div class="row" name="top">
 						<h1>Steven Emanuel Roomberg</h1>
 						<h2>Software Engineer</h2>
 						<img src="" /><!--Here is where a picture of me will go-->
 						<p>Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb. </p>
+						<p>Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing. </p>
+						<p>The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee. </p>
+						<p>The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee. </p>
+						<p>Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass. </p>
 						<p>Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'? </p>
+						<p>Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.  </p>
 					</div>
+					<!--These divs will be removed once content is added--><div class="separator"></div>
+					<a name="about"></a>
 					<div class="row" name="about">
+						<h2>About Me</h2>
 						<p>Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb. </p>
 						<p>Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing. </p>
+						<p>The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee. </p>
+						<p>The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee. </p>
+						<p>Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass. </p>
+						<p>Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'? </p>
+						<p>Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.  </p>
 					</div>
+					<!--These divs will be removed once content is added--><div class="separator"></div>
+					<a name="education"></a>
+					<div class="row" name="education">
+						<h2>Education</h2>
+						<div class="col-md-8">
+							<h3>University of Pittsburgh</h3>
+							<h4>Bachelors of Science, Computer Science</h4>
+						</div>
+						<div class="col-md-4">
+							<h4>Class of 2017</h4>
+						</div>
+					</div>
+					<!--These divs will be removed once content is added--><div class="separator"></div>
+					<a name="resume"></a>
 					<div class="row" name="resume">
-						<div class="row">
-							<h2>Education</h2>
-							<div class="col-md-8">
-								<h3>University of Pittsburgh</h3>
-								<h4>Bachelors of Science, Computer Science</h4>
-							</div>
-							<div class="col-md-4">
-								<h4>Class of 2017</h4>
-							</div>
-						</div>
-						<div class="row">
-							<h2>Professional Experience</h2>
-							<!--Some work experience-->
-						</div>
+						<h2>Professional Experience</h2>
 					</div>
+					<!--These divs will be removed once content is added--><div class="separator"></div>
+					<a name="portfolio"></a>
+					<div class="row" name="portfolio">
+						<h2>Portfolio</h2>
+					</div>
+					<!--These divs will be removed once content is added--><div class="separator"></div>
+					<a name="contact"></a>
 					<div class="row" name="contact">
-						<!--Contact form-->
+						<h2>Contact</h2>
+						<!--JS-enabled Contact form-->
 					</div>
 				</div>
 			</div>
